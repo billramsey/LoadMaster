13246192ab871e88fc1057052cd887d3fd61c72f
Felix <felix2feng@gmail.com>
2016-06-27 12:20:30 -0700

    Externalized master code

---
+++ master/master.js
@@ -0,0 +1,32 @@
+// All master logic incl. Communication between web server and worker, worker spin up and wind down
+
+// Dependencies
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// Modules
+const masterController = require('./master_controller.js');
+
+// DEPENDENCY: [TODO] Need to update with correct port number
+const port = process.env.port || 8000;
+
+// Start Express Server
+const app = express();
+app.set('port', port);
+
+// Middleware
+app.use(bodyParser.json());
+
+// Handle POST request from the web server
+app.post('/api/master', masterController.handleJobFromWebServer);
+
+// Handle POST request for jobs from the worker
+app.post('/api/requestJob', masterController.requestJob);
+
+// Handle completion POST request from the worker
+app.post('/api/complete', masterController.complete);
+
+// Server listens at specified port
+app.listen(app.get('port'), () => {
+  console.log(`Master server listening to port ${app.get('port')}`);
+});
---
+++ master/master_controller.js
@@ -0,0 +1,136 @@
+// Handle all requests to master.js server
+
+/*
+CURRENT MVP IMPLEMENTATION SPECIFICATIONS
+--All work is handled in the webServer function
+--Server reponds when all tasks have been completed
+--All tasks live within one request function
+--Ideal situation: EC2 instances are spun up every time a request is needed
+--NOT DESIGNED FOR SCALE
+--Error handling - Redistribute work to workers that have gone offline
+*/
+
+// ASSUMPTIONS
+const tasksPerJob = 10; // Arbitrary number of actions per job
+
+// Dependencies
+const dockerConnection = require('../config/docker-config');
+const util = require('../helper/utils');
+
+// Modules
+const Queue = require('../helper/queue');
+const divide = require('..//helper/divide');
+const helpers = require('../helper/helpers');
+const Scenario = require('../models/ScenariosModel');
+
+// Global Variables
+const jobQueue = new Queue();
+const status = {
+  workerCount: 0,
+};
+let results = [];
+let currentResultAverage = 0;
+let currentscenarioID;
+let currentUser;
+let totalJobs = 0;
+
+// Handle incoming requests from Web Server
+const handleJobFromWebServer = (req, res) => {
+  // Individual Task Unit
+  currentscenarioID = req.body.scenarioID;
+  currentUser = req.body.id_user;
+
+  const task = {
+    // TODO: Verify Scenario ID is correctly sent
+    scenarioID: req.body.scenarioID,
+    scenario: req.body.scenarioName,
+    user: req.body.id_user,
+    targetUrl: req.body.targetUrl,
+    script: req.body.script,
+  };
+
+  // TODO: Get scenario ID and pass onto task
+
+  // Split up jobs into chunks and place into job queue
+  const spawnCount = +req.body.spawnCount;
+  totalJobs = spawnCount;
+
+  const jobsToAdd = divide(spawnCount, tasksPerJob);
+  const jobBundle = helpers.bundleTasks(task, tasksPerJob);
+
+  for (let i = 0; i < jobsToAdd; i++) {
+    jobQueue.addToQueue(jobBundle);
+  }
+
+  console.log('queue is', jobQueue.items);
+  console.log('total jobs', totalJobs);
+
+  // Wind up number of requested workers
+  const workers = req.body.workers;
+  // TODO: CHRIS TO PROVIDE CODE TO WIND UP WORKERS
+  for (let j = 1; j <= workers; j++) {
+    status.workerCount = j;
+    const workerName = 'worker'.concat(status.workerCount);
+    console.log('creating ' + workerName);
+    util.createContainer(dockerConnection, 'node-sender', workerName);
+  }
+
+  res.status(201).send('webserver post request received for ' + workers + ' workers');
+};
+
+const complete = (req, res) => {
+  console.log('Received POST complete request!', req.body);
+  // Add to completed jobs list
+  console.log('This is results before the concat', results);
+  results = results.concat(req.body);
+  /*
+  [
+    runresults: {
+      scenarioTime: timeToRunScenarioInMilliseconds,
+      transactionTimes: [
+        [path, statusCode, elapsedTime, dataSizeInBytes, 'GET'],
+      ]
+    },
+    runresults: {
+      scenarioTime: timeToRunScenarioInMilliseconds,
+      transactionTimes: [
+        [path, statusCode, elapsedTime, dataSizeInBytes, 'GET'],
+      ]
+    },
+  ]
+  */
+
+  console.log('These are our results length', results.length);
+  console.log('Total jobs', totalJobs);
+  if (results.length === totalJobs) {
+    console.log('We are done!');
+
+    // TODO do post request with results to the web server
+
+    let totalTime = 0;
+    const resultLength = results.length;
+    for (let k = 0; k < results.length; k++) {
+      totalTime += results[k].scenarioTime;
+    }
+    const averageTime = totalTime / resultLength;
+    // Get specific scenario from the database
+    Scenario.where({ scenarioID: currentscenarioID, id_user: currentUser })
+      .save({ averageActionTime: averageTime });
+  }
+  res.status(200).send();
+};
+
+const requestJob = (req, res) => {
+  console.log('Got a post request on master server! Queue is', jobQueue.items);
+  // Check if jobs are available
+  if (jobQueue.checkLength() > 0) {
+    const job = jobQueue.takeNext();
+    res.json({ job });
+  } else {
+    // If no jobs available send 0
+    res.send('done');
+  }
+};
+
+
+module.exports = { handleJobFromWebServer, complete, requestJob };
